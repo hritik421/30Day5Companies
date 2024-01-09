353077ec56ffcbb768e944a84617b1290f9f3c2d
hritik421 <95011875+hritik421@users.noreply.github.com>
2024-01-09 20:49:09 +0530

    Update Find the Winner of the Circular Game.cpp

--- Microsoft/Find the Winner of the Circular Game.cpp
+++ Microsoft/Find the Winner of the Circular Game.cpp
@@ -53,3 +53,14 @@ public:
         return head->data;
     }
 };
+
+// Recursive solution
+
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        if (n==1) return n;
+        int ans = (findTheWinner(n-1,k)+k)%n;
+        return ans == 0 ? n : ans;
+    }
+};
